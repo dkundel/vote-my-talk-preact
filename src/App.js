90c8cb70b74b51c68ce9b5bc894f6e85086ca30d
Dominik Kundel <dominik.kundel@gmail.com>
2018-09-11 13:13:59 +0200

    :sparkles: Setup for demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './logo.svg';
+import Resources from './components/Resources';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentValue: 0,
+      submitted: Boolean(localStorage.getItem('voted')) || false,
+    };
+    this.submitRating = this.submitRating.bind(this);
+  }
+
+  async submitRating() {
+    const url =
+      'https://cors-anywhere.herokuapp.com/https://dramatic-sidewalk-3548.twil.io/vote-talk';
+    const event = 'reactalicante-webcomponents';
+    const vote = this.state.currentValue;
+    if (this.state.submitted) {
+      return;
+    }
+
+    try {
+      const body = JSON.stringify({ vote, event });
+      const resp = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body,
+      });
+      console.log(resp);
+      localStorage.setItem('voted', 'true');
+      this.setState({ submitted: true });
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   render() {
+    const { submitted } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="main">
+        <h1>Thanks for watching my talk!</h1>
+        <img
+          src="https://media.giphy.com/media/l3V0lsGtTMSB5YNgc/giphy.gif"
+          alt="daning panda GIF"
+        />
+        <p className="tag-line">
+          I would really appreciate your feedback. Please click on the amount of{' '}
+          <span role="img" aria-label="panda">
+            🐼
+          </span>{' '}
+          you would award for this talk!
         </p>
+        {!submitted && (
+          <button onClick={this.submitRating}>Submit Rating</button>
+        )}
+        {submitted && (
+          <div>
+            <p className="thank-you">
+              Thanks for voting this talk with
+              <span> {this.state.currentValue} </span> pandas!
+            </p>
+          </div>
+        )}
+        <Resources />
       </div>
     );
   }
---
+++ src/components/Resources.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Resources = () => {
+  return (
+    <div>
+      <h2>Resources</h2>
+      <ul>
+        <li>
+          <a href="https://twitter.com/dkundel">@dkundel</a>
+        </li>
+        <li>
+          <a href="https://github.com/dkundel">github.com/dkundel</a>
+        </li>
+        <li>
+          <a href="https://github.com/dkundel/emoji-rating">
+            Web Component Code for emoji-rating
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/dkundel/vote-my-talk-preact">
+            Code for this demo
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twilio.com/blog/2018/02/creating-and-publishing-web-components-with-stencil.html">
+            Creating web components in Stencil
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Resources;
