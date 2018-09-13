eac8d2ef96306cae45cd9f83b8dd842a3ab7fa13
Dominik Kundel <dominik.kundel@gmail.com>
2018-09-13 22:30:33 +0200

    :bug: Fix production bundling

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,8 @@
 const rewirePreact = require('react-app-rewire-preact');
+const rewireUglifyjs = require('react-app-rewire-uglifyjs');
 
 module.exports = function override(config, env) {
-  // config = rewirePreact(config, env);
+  config = rewirePreact(config, env);
+  config = rewireUglifyjs(config);
   return config;
 };
